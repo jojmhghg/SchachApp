4386bcaec08ef4c493fa4519bb6b2743d5285bfa
Tim Meyer <timeyer93@googlemail.com>
2018-07-17 11:04:41 +0200

    login & register acitivities hinzugefügt

--- app/src/main/java/htw/de/schachapp/LoginActivity.java
+++ app/src/main/java/htw/de/schachapp/LoginActivity.java
@@ -1,22 +1,18 @@
 package htw.de.schachapp;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -25,58 +21,32 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
         Button loginButton = (Button)findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText email = (EditText)findViewById(R.id.emailText);
+                EditText email = (EditText)findViewById(R.id.emailInput);
                 EditText password = (EditText)findViewById(R.id.passwordText);
 
                 mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
                         .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                TextView resultLabel = (TextView)findViewById(R.id.resultLabel);
                                 if (task.isSuccessful()) {
                                     FirebaseUser user = mAuth.getCurrentUser();
-                                    resultLabel.setText(user.getUid());
+                                    //TODO: zu Startseite navigieren
                                 } else {
-                                    resultLabel.setText("failure");
+                                    //TODO: Fehler anzeigen
                                 }
                             }
                         });
             }
         });
 
-        Button registerButton = (Button)findViewById(R.id.registerButton);
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText email = (EditText)findViewById(R.id.emailText);
-                EditText password = (EditText)findViewById(R.id.passwordText);
-
-                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                TextView resultLabel = (TextView)findViewById(R.id.resultLabel);
-                                if (task.isSuccessful()) {
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    resultLabel.setText(user.getUid());
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    /*Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();*/
-                                    resultLabel.setText("failure");
-                                }
-                            }
-                        });
-            }
-        });
-
+        /*
         Button logoutButton = (Button)findViewById(R.id.logoutButton);
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,21 +56,30 @@ public class LoginActivity extends AppCompatActivity {
                 resultLabel.setText("ausgeloggt");
             }
         });
+        */
+
+        Button registerButton = (Button)findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(startIntent);
+            }
+        });
 
         Button resetButton = (Button)findViewById(R.id.resetButton);
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText email = (EditText)findViewById(R.id.emailText);
+                EditText email = (EditText)findViewById(R.id.emailInput);
                 mAuth.sendPasswordResetEmail(email.getText().toString())
                         .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
-                                TextView resultLabel = (TextView)findViewById(R.id.resultLabel);
                                 if (task.isSuccessful()) {
-                                    resultLabel.setText("reset successful");
+                                    //TODO: successful Nachricht anzeigen
                                 } else {
-                                    resultLabel.setText("reset failure");
+                                    //TODO: Fehlernachricht anezeigen
                                 }
                             }
                         });
@@ -114,12 +93,8 @@ public class LoginActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
-        TextView resultLabel = (TextView)findViewById(R.id.resultLabel);
         if(currentUser != null){
-            resultLabel.setText(currentUser.getUid());
-        }
-        else{
-            resultLabel.setText("niemand angemeldet!");
+            //TODO: Zu Startseite navigieren
         }
     }
 }
---
+++ app/src/main/java/htw/de/schachapp/RegisterActivity.java
@@ -0,0 +1,56 @@
+package htw.de.schachapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+        Button registerButton = (Button)findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText email = (EditText)findViewById(R.id.emailInput);
+                EditText password = (EditText)findViewById(R.id.passwordText);
+
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    //TODO: Zu Login navigieren und dort Bestätigung anzeigen
+                                } else {
+                                    //TODO: Fehler anzeigen
+                                }
+                            }
+                        });
+            }
+        });
+
+        Button returnButton = (Button)findViewById(R.id.returnButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+}
