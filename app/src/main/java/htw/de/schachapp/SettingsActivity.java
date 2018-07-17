d7ee7e5e41f736ed96e0b44853afd7187aa350f2
Tim Meyer <timeyer93@googlemail.com>
2018-07-17 14:42:48 +0200

    firestore listener added for Settings

--- app/src/main/java/htw/de/schachapp/SettingsActivity.java
+++ app/src/main/java/htw/de/schachapp/SettingsActivity.java
@@ -1,13 +1,20 @@
 package htw.de.schachapp;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.functions.FirebaseFunctions;
 
 import java.util.HashMap;
@@ -15,15 +22,42 @@ import java.util.Map;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     private FirebaseFunctions mFunctions;
-    FirebaseFirestore db;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        mAuth = FirebaseAuth.getInstance();
         mFunctions = FirebaseFunctions.getInstance();
         db = FirebaseFirestore.getInstance();
+
+        Log.d("SchachApp", "test");
+
+        final DocumentReference docRef = db.collection("user").document(mAuth.getUid());
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+
+                EditText email = (EditText)findViewById(R.id.emailInput);
+                if (e != null) {
+                    Log.d("SchachApp", "snapshot failed");
+                    email.setText("");
+                    return;
+                }
+
+                if (snapshot != null && snapshot.exists()) {
+                    Log.d("SchachApp", "Current data: " + snapshot.getData());
+                    email.setText("Current data: " + snapshot.getData().get("username"));
+                } else {
+                    Log.d("SchachApp", "Current data: null");
+                    email.setText(mAuth.getUid());
+                }
+            }
+        });
 
         Button saveButton = (Button)findViewById(R.id.saveButton);
         saveButton.setOnClickListener(new View.OnClickListener() {
